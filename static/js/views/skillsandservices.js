4ce7c9ba45d8ac9de8c931af1731131064ee9fdb
Samuel Langley <samuellangley@intelligencefusion.co.uk>
2023-03-11 22:08:30 +0000

    github pages

--- static/js/views/skillsandservices.js
+++ static/js/views/skillsandservices.js
@@ -1,4 +1,5 @@
 import AbstractView from "./AbstractView.js";
+import { u } from '../lib.js';
 
 export default class extends AbstractView {
     constructor(params) {
@@ -9,17 +10,17 @@ export default class extends AbstractView {
     async getHtml() {
         return `
         <!-- Skills -->
-        
-        
+
+
         <section class="skills-section" id="skills">
             <h2 class="section-title skills-text">SKILLS</h2>
             <span class="section-subtitle skills-subtxt">What am I good at?</span>
         </div>
-        
-        
+
+
             <div class="skills-container container grid">
                 <div>
-        
+
         <!-- Skills Section 1 -->
             <div class="skills-content skills-open">
                         <div class="skills-header">
@@ -28,10 +29,10 @@ export default class extends AbstractView {
                         <h1 class="skills-titles">Frontend Developer</h1>
                         <span class="skills-subtitle">Under a Year</span>
                         </div>
-        
+
                         <i class="uil uil-angle-right skills-arrow"></i>
                         </div>
-                
+
                 <div class="skills-list grid">
                     <div class="skills-data">
                         <div class="skills-titles">
@@ -42,7 +43,7 @@ export default class extends AbstractView {
                             <span class="skills-percentage skills-html"></span>
                         </div>
                     </div>
-        
+
                     <div class="skills-data">
                         <div class="skills-titles">
                             <h3 class="skills-name">CSS</h3>
@@ -52,7 +53,7 @@ export default class extends AbstractView {
                             <span class="skills-percentage skills-css"></span>
                         </div>
                     </div>
-        
+
                     <div class="skills-data">
                         <div class="skills-titles">
                             <h3 class="skills-name">Javascript</h3>
@@ -61,7 +62,7 @@ export default class extends AbstractView {
                         <div class="skills-bar">
                             <span class="skills-percentage skills-javascript"></span>
                         </div>
-                        
+
                         <div class="skills-data">
                             <div class="skills-titles">
                                 <h3 class="skills-name">React/Vue</h3>
@@ -75,7 +76,7 @@ export default class extends AbstractView {
             </div>
             </div>
             <br>
-        
+
             <!-- Skills Section 2 -->
             <div class="skills-content skills-close">
                         <div class="skills-header">
@@ -84,10 +85,10 @@ export default class extends AbstractView {
                         <h1 class="skills-titles">Web Designer</h1>
                         <span class="skills-subtitle">Over 3 Years</span>
                         </div>
-        
+
                         <i class="uil uil-angle-right skills-arrow"></i>
                         </div>
-                
+
                 <div class="skills-list grid">
                     <div class="skills-data">
                         <div class="skills-titles">
@@ -98,7 +99,7 @@ export default class extends AbstractView {
                             <span class="skills-percentage skills-ui"></span>
                         </div>
                     </div>
-        
+
                     <div class="skills-data">
                         <div class="skills-titles">
                             <h3 class="skills-name">User Experience</h3>
@@ -108,7 +109,7 @@ export default class extends AbstractView {
                             <span class="skills-percentage skills-ux"></span>
                         </div>
                     </div>
-        
+
                     <div class="skills-data">
                         <div class="skills-titles">
                             <h3 class="skills-name">Creation of Original Content</h3>
@@ -117,7 +118,7 @@ export default class extends AbstractView {
                         <div class="skills-bar">
                             <span class="skills-percentage skills-design"></span>
                         </div>
-                        
+
                         <div class="skills-data">
                             <div class="skills-titles">
                                 <h3 class="skills-name">Figma/Canva/Balsamiq</h3>
@@ -131,9 +132,9 @@ export default class extends AbstractView {
             </div>
             </div>
             <br>
-        
+
                 <!-- Skills Section 3 -->
-        
+
             <div class="skills-content skills-close">
                         <div class="skills-header">
                             <i class="uil uil-server-connection skills-icon"></i>
@@ -143,7 +144,7 @@ export default class extends AbstractView {
                     </div>
                     <i class="uil uil-angle-right skills-arrow"></i>
                     </div>
-                
+
                 <div class="skills-list grid">
                     <div class="skills-data">
                         <div class="skills-titles">
@@ -154,7 +155,7 @@ export default class extends AbstractView {
                             <span class="skills-percentage skills-python"></span>
                         </div>
                     </div>
-        
+
                     <div class="skills-data">
                         <div class="skills-titles">
                             <h3 class="skills-name">Node.JS</h3>
@@ -164,17 +165,17 @@ export default class extends AbstractView {
                             <span class="skills-percentage skills-php"></span>
                         </div>
                     </div>
-        
+
                     <div class="skills-data">
                         <div class="skills-titles">
                             <h3 class="skills-name">PHP</h3>
                             <span class="skills-number"></span>
                         </div>
-                        
+
                         <div class="skills-bar">
                             <span class="skills-percentage skills-node"></span>
                         </div>
-                        
+
                         <div class="skills-data">
                             <div class="skills-titles">
                                 <h3 class="skills-name">Vue / Angular</h3>
@@ -187,7 +188,7 @@ export default class extends AbstractView {
                 </div>
         </div>
         </div>
-        
+
         </section>
 
         <hr class="hr-pager-2">
@@ -224,11 +225,11 @@ export default class extends AbstractView {
             <li class="services-modal-service">
                 <i class="uil uil-check-circle services-modal-icon"></i>
                 <p class="p-services">I will help gather data on your service users.</p>
-            </li>            
+            </li>
             <li class="services-modal-service">
                 <i class="uil uil-check-circle services-modal-icon"></i>
                 <p class="p-services">I will help you build your web/app design based on the psychology of your users.</p>
-            </li>            
+            </li>
             <li class="services-modal-service">
                 <i class="uil uil-check-circle services-modal-icon"></i>
                 <p class="p-services">I will create vision boards, wireframes and prototypes to show customers the best way to gain traction.</p>
@@ -261,11 +262,11 @@ export default class extends AbstractView {
             <li class="services-modal-service">
                 <i class="uil uil-check-circle services-modal-icon"></i>
                 <p class="p-services">I will use the latest and best versions of tech in order to build your websites and apps.</p>
-            </li>            
+            </li>
             <li class="services-modal-service">
                 <i class="uil uil-check-circle services-modal-icon"></i>
                 <p class="p-services">I will work in collaboration with other developers to improve use and function of your website and apps.</p>
-            </li>            
+            </li>
             <li class="services-modal-service">
                 <i class="uil uil-check-circle services-modal-icon"></i>
                 <p class="p-services">I will use the tools I have to optimise running speed and troubleshoot as necessary.</p>
@@ -298,11 +299,11 @@ export default class extends AbstractView {
             <li class="services-modal-service">
                 <i class="uil uil-check-circle services-modal-icon"></i>
                 <p class="p-services">I will write you a branding design plan in order to maximise customer attraction.</p>
-            </li>            
+            </li>
             <li class="services-modal-service">
                 <i class="uil uil-check-circle services-modal-icon"></i>
                 <p class="p-services">I will design your website and logo to fit your specific branding strategy to market products and services.</p>
-            </li>            
+            </li>
             <li class="services-modal-service">
                 <i class="uil uil-check-circle services-modal-icon"></i>
                 <p class="p-services">I will use my skills to elevate your marketing strategy using research and collaboration.</p>
@@ -335,11 +336,11 @@ export default class extends AbstractView {
             <li class="services-modal-service">
                 <i class="uil uil-check-circle services-modal-icon"></i>
                 <p class="p-services">I will design you a beautiful website to fit your brand.</p>
-            </li>            
+            </li>
             <li class="services-modal-service">
                 <i class="uil uil-check-circle services-modal-icon"></i>
                 <p class="p-services">I will create multiple options for web-designs to give you choice.</p>
-            </li>            
+            </li>
             <li class="services-modal-service">
                 <i class="uil uil-check-circle services-modal-icon"></i>
                 <p class="p-services">I will design your website in order to attract your ideal customer base.</p>
@@ -351,7 +352,7 @@ export default class extends AbstractView {
 
 </div>
 </section>
-        
+
         `;
     }
 }
